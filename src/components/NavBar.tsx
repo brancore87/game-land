bc74cb42505d79f9ae83b7fd5b65f850eceac3b4
brancore87 <brancore87@gmail.com>
2023-07-03 22:59:11 +0530

    Update page title and logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,7 @@
 import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
-
+import logo from '../assets/logo.png'
 
 interface Props {
     onSearch: (searchText: string) => void;
@@ -11,7 +10,7 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
     return (
         <HStack padding="10px">
-            <Image src={logo} boxSize='60px' />
+            <Image src={logo} boxSize='50px' />
             <SearchInput onSearch={onSearch} />
             <ColorModeSwitch />
         </HStack>
