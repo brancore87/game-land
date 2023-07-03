8d07bac2ecf53fb9624efcaf29c368a38fef373c
brancore87 <brancore87@gmail.com>
2023-07-03 11:01:37 +0530

    Displaying Critic Score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,20 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+    score: number;
+}
+
+
+const CriticScore = ({ score }: Props) => {
+    let color = score > 75 ? 'green' : score > 60 ? 'yello' : '';
+    return (
+        <Badge
+            colorScheme={color}
+            fontSize="15px"
+            paddingX={2}
+            borderRadius="5px" >{score}
+        </Badge>
+    )
+}
+
+export default CriticScore
