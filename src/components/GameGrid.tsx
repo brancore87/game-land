ed68c5957a5b8f373b931c252060e43c40a853c0
brancore87 <brancore87@gmail.com>
2023-07-06 08:21:13 +0530

    Refactoring - fetch games with React Query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,8 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
 
 
@@ -16,7 +15,7 @@ const GameGrid = ({ gameQuery }: Props) => {
     const { data, error, isLoading } = useGames(gameQuery);
     const skeletons = [1, 2, 3, 4, 5, 6];
 
-    if (error) return <Text>{error}</Text>
+    if (error) return <Text>{error.message}</Text>
 
     return (
         <SimpleGrid
@@ -28,7 +27,7 @@ const GameGrid = ({ gameQuery }: Props) => {
                 <GameCardContainer key={skeleton}>
                     <GameCardSkeleton />
                 </GameCardContainer>)}
-            {data.map((game) => (
+            {data?.results.map((game) => (
                 <GameCardContainer key={game.id}>
                     <GameCard game={game} />
                 </GameCardContainer>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,38 @@
 import axios from "axios";
 
+
+
+export interface FetchResponse<T>{
+    count: number;
+    results: T[];
+}
+
+
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
         key: '17f2396a468e41e4b416fb47a965c480'
     }
 })
+
+// const axiosInstance = axios.create({
+//     baseURL: 'https://api.rawg.io/api',
+//     params: {
+//                  key: '17f2396a468e41e4b416fb47a965c480'
+//              }
+// })
+
+// class APIClient<T> {
+//     endpoint: string;
+
+//     constructor(endpoint: string) {
+//         this.endpoint = endpoint
+//     }
+
+//     getAll = () => {
+//         return axiosInstance
+//         .get<T[]>(this.endpoint)
+//         .then(res => res.data);
+//     }
+// }
+
