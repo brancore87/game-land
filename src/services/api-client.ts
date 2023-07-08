6d04e7c14ee3bf48595055b8472e7883e0e24fe2
brancore87 <brancore87@gmail.com>
2023-07-08 13:33:38 +0530

    env baseurl config

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 }
 
 const axiosInstance = axios.create({
-    baseURL: "https://api.rawg.io/api",
+    baseURL:import.meta.env.VITE_BASE_URL,
     params: {
         key:import.meta.env.VITE_API_KEY
     }
